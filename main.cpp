8dff572cfd87e6132d608a1e229361d430d8a00e
JonathanDiaz12 <diaz.j023958@stu.sanjac.edu>
2025-02-03 19:46:08 +0000

    Added the Project information comments at the top

--- main.cpp
+++ main.cpp
@@ -1,3 +1,9 @@
+//************************************************************************************
+//Author: Jonathan Diaz
+//Assignment: Banking Systems Project
+//Description: This program is meant to create a table like system where a user is asked to put a number 1-4 to determine what they want to do with their banking solutions.
+// Chosing "1" will create a deposit, "2" a withdrawl, "3" a balance check and "4" will quit the program.
+//************************************************************************************
 #include <iomanip>
 #include <iostream>
 #include <string>
